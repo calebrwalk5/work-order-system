1c5ef9318e1fbc839baf1d2a050925d8d31c98f9
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2020-03-04 10:02:39 -0500

    Update App.js

--- scripts/App.js
+++ scripts/App.js
@@ -1,7 +1,7 @@
-function save() {
-    var message = document.getElementById("inputText").value;
-    fs.writeFile("message.txt", message) {
-        if (err) throw err;
-    console.log('It\'s saved!');
-  });
+function download(text, name, type) {
+  var text = document.getElementById("setText");
+  var a = document.getElementById("a");
+  var file = new Blob([text], {type: type});
+  a.href = URL.createObjectURL(file);
+  a.download = name;
 }
